629b7ffcb4ceb95d68a0a6c937589bda402f4354
Russel Arbore <russel.jma@gmail.com>
2022-07-02 13:07:19 -0500

    Fixed window movement order

--- config.h
+++ config.h
@@ -105,10 +105,10 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY|ControlMask,           XK_k,      focusmon,       {.i = -1 } },
-	{ MODKEY|ControlMask,           XK_j,      focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask|ControlMask, XK_k,      tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask|ControlMask, XK_j,      tagmon,         {.i = +1 } },
+	{ MODKEY|ControlMask,           XK_k,      focusmon,       {.i = +1 } },
+	{ MODKEY|ControlMask,           XK_j,      focusmon,       {.i = -1 } },
+	{ MODKEY|ShiftMask|ControlMask, XK_k,      tagmon,         {.i = +1 } },
+	{ MODKEY|ShiftMask|ControlMask, XK_j,      tagmon,         {.i = -1 } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
